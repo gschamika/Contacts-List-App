a0de3e5c438ecb03d6824d74182286770569be90
Sumudu Chamika <97242725+gschamika@users.noreply.github.com>
2024-05-31 21:04:10 +0530

    change 12

--- app/src/main/java/com/example/contactslistapp/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/contactslistapp/RecyclerViewAdapter.java
@@ -7,7 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Filter;
-import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
@@ -119,7 +119,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter <RecyclerViewAdapt
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         TextView profileLetter, name, phone;
-        ImageButton deleteButton;
+        ImageView deleteButton;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
